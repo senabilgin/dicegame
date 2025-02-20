b7e06b88234345a447ecb399edb6e2a978e98fc8
senabilgin <49808970+senabilgin@users.noreply.github.com>
2025-02-20 14:50:00 -0500

    dice class

---
+++ Dice.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Dice
+{
+        //fields
+        private const int numOfSides = 6;
+        private Random dice;
+        private int topSide;
+
+        //proprities 
+        public int TopSide{
+            get { return topSide;}
+        }
+
+        //constructors
+        public Dice(int numOfSides)
+
+
+}
